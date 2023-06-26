68fc6172d83ee644093e540e5f772bde82a0dc0d
Jimmy <james.a.davies7@gmail.com>
2023-06-26 11:57:04 +1000

    Adding heading to tile component

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,24 +1,40 @@
 // import cn from 'classnames';
 import { createElement, FC } from 'react';
 
-export interface Props {
+import { createHeadingId } from '../../utils/id-generators';
+import Heading from '../Heading/Heading';
+
+interface Props {
   /** optional override for the html element, div by default */
   as?: string;
   /** this is the contents of the heading element */
   children: React.ReactNode;
+  /** heading or title to describe the contents of the tile */
+  heading: string;
   /** a unqiue id for the element */
   id: string;
 }
 
-const Tile: FC<Props> = ({ as = 'div', children, id }) => {
+const Tile: FC<Props> = ({ as = 'div', children, heading, id }) => {
+  const headingId = createHeadingId(id);
+
+  const content = (
+    <>
+      <Heading id={headingId}>
+        {heading}
+      </Heading>
+      {children}
+    </>
+  );
 
   return createElement(
     as,
     {
+      ariaLabelledBy: headingId,
       className: '',
       id,
     },
-    children,
+    content,
   );
 };
 
